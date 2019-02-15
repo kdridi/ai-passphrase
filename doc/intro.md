564d3232bfd20bb6f8e09e17982945c647cc2444
Karim DRIDI <karim.dridi@gmail.com>
2019-02-15 14:17:32 +0100

    First commit!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ai-passphrase
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ai_passphrase/core_test.clj
@@ -0,0 +1,7 @@
+(ns ai-passphrase.core-test
+  (:require [clojure.test :refer :all]
+            [ai-passphrase.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
